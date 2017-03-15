f52b9b0b8473de239e3d71d3841f1a04ac955ad6
Redouane <m.redouane@hotmail.com>
2017-03-15 13:12:46 +0000

    Add files via upload

---
+++ Game/MyGame/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿using System;
+
+namespace MyGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Screen = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Block = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Block2 = new System.Windows.Forms.PictureBox();
+            this.Screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Block)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Block2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Screen
+            // 
+            this.Screen.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Screen.Controls.Add(this.Block2);
+            this.Screen.Controls.Add(this.pictureBox6);
+            this.Screen.Controls.Add(this.pictureBox5);
+            this.Screen.Controls.Add(this.pictureBox4);
+            this.Screen.Controls.Add(this.pictureBox3);
+            this.Screen.Controls.Add(this.pictureBox2);
+            this.Screen.Controls.Add(this.pictureBox1);
+            this.Screen.Controls.Add(this.Block);
+            this.Screen.Controls.Add(this.Player);
+            this.Screen.Location = new System.Drawing.Point(0, 0);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(892, 341);
+            this.Screen.TabIndex = 0;
+            this.Screen.Paint += new System.Windows.Forms.PaintEventHandler(this.Screen_Paint);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::MyGame.Properties.Resources.cloud;
+            this.pictureBox6.Location = new System.Drawing.Point(652, 28);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(75, 50);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::MyGame.Properties.Resources.cloud2;
+            this.pictureBox5.Location = new System.Drawing.Point(457, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(155, 110);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::MyGame.Properties.Resources.cloud;
+            this.pictureBox4.Location = new System.Drawing.Point(376, 56);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(75, 50);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MyGame.Properties.Resources.cloud;
+            this.pictureBox3.Location = new System.Drawing.Point(313, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 50);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MyGame.Properties.Resources.cloud2;
+            this.pictureBox2.Location = new System.Drawing.Point(152, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(155, 110);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MyGame.Properties.Resources.cloud;
+            this.pictureBox1.Location = new System.Drawing.Point(70, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Block
+            // 
+            this.Block.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Block.Location = new System.Drawing.Point(152, 287);
+            this.Block.Name = "Block";
+            this.Block.Size = new System.Drawing.Size(207, 16);
+            this.Block.TabIndex = 1;
+            this.Block.TabStop = false;
+            this.Block.Click += new System.EventHandler(this.Block_Click);
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.Image = global::MyGame.Properties.Resources.stand;
+            this.Player.InitialImage = global::MyGame.Properties.Resources.stand;
+            this.Player.Location = new System.Drawing.Point(32, 16);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(16, 32);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel2.BackgroundImage = global::MyGame.Properties.Resources.floor;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.ForeColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(0, 340);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(890, 24);
+            this.panel2.TabIndex = 1;
+            // 
+            // Block2
+            // 
+            this.Block2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Block2.Location = new System.Drawing.Point(418, 239);
+            this.Block2.Name = "Block2";
+            this.Block2.Size = new System.Drawing.Size(143, 18);
+            this.Block2.TabIndex = 8;
+            this.Block2.TabStop = false;
+            this.Block2.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(890, 364);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Screen);
+            this.Name = "Form1";
+            this.Text = "myGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Block)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Block2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Block;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Panel Screen;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox Block2;
+    }
+}
+
---
+++ Game/MyGame/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    public partial class Form1 : Form
+    {
+
+        bool right, left;
+        bool jump;
+        int G = 20;
+        int Force;
+        int index = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Block.Top = Screen.Height - Block.Height; // Start pos
+            //Player.Top = Screen.Height - Player.Height; //Player pos
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            index++;
+            // Gif replay
+            if (right == true && index %15 == 0)
+            {
+                //Player.Image = Image.FromFile("walk_r.gif");
+                Player.Image = Image.FromFile("walk_r.gif");
+            }
+            if (left == true && index % 15 == 0)
+            {
+                Player.Image = Image.FromFile("walk_l.gif");
+            }
+
+
+
+            // Side collision
+            if (Player.Right > Block.Left && Player.Left< Block.Right - Player.Width && Player.Bottom< Block.Bottom && Player.Bottom>Block.Top)
+            {
+                right = false;
+            }
+            if (Player.Left < Block.Right && Player.Right > Block.Left + Player.Width && Player.Bottom < Block.Bottom && Player.Bottom > Block.Top)
+            {
+                left = false;
+            }
+
+            //***************
+            if (right == true){ Player.Left += 5;}
+            if (left == true) { Player.Left -= 5;}
+
+            // Falling if jumping
+            if (jump == true)
+            {
+                Player.Top -= Force;
+                Force -= 1;
+            }
+
+            if (Player.Top + Player.Height >= Screen.Height)
+            {
+                Player.Top = Screen.Height - Player.Height; // Stop the falling to bottom
+                if (jump == true)
+                {
+                    Player.Image = Image.FromFile("stand.png");
+                }
+
+                jump = false; 
+            }
+            else
+            {
+                Player.Top += 5; // Falling
+            } 
+
+            // Top collision
+            if (Player.Left + Player.Width- 1 > Block.Left && Player.Left + Player.Width +5 < Block.Left + Block.Width + Player.Width && Player.Height + Player.Top >= Block.Top && Player.Top < Block.Top)
+            {
+                /*Player.Top = Screen.Height - Block.Height - Player.Height;
+                Force = 0;
+                if (jump == true)
+                {
+                    Player.Image = Image.FromFile("stand.png");
+                }*/
+                jump = false;
+                Force = 0;
+                Player.Top = Block.Location.Y - Player.Height;
+            }
+
+            // Head collision
+            if (Player.Left + Player.Width - 1 > Block.Left && Player.Left + Player.Width + 5 < Block.Left + Block.Width + Player.Width && Player.Top - Block.Bottom <=10 && Player.Top - Block.Top > -10)
+            {
+                Force = -1;
+                
+            }
+            if (Player.Left + Player.Width - 1 > Block2.Left && Player.Left + Player.Width + 5 < Block2.Left + Block2.Width + Player.Width && Player.Top - Block2.Bottom <= 10 && Player.Top - Block2.Top > -10)
+            {
+                Force = -1;
+
+            }
+            if (Block2.Bottom - Player.Top >= 10 && Player.Top - Block2.Top < -10)
+            {
+                Force = 1;
+
+            }
+
+
+            //***************
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = true;
+                //Player.Image = Image.FromFile("walk_r.gif");
+            }
+            if (e.KeyCode == Keys.Left) { left = true;
+                //Player.Image = Image.FromFile("walk_l.gif");
+            }
+            if (e.KeyCode == Keys.Escape) { this.Close(); } //Escape -> Exit
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    Force = G;
+                    Player.Image = Image.FromFile("jump.png");
+                }
+            }
+
+        }
+
+        private void Block_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Screen_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = false;
+                Player.Image = Image.FromFile("stand.png");
+            }
+            if (e.KeyCode == Keys.Left) { left = false;
+                Player.Image = Image.FromFile("stand.png");
+            }
+           
+        }
+    }
+}
